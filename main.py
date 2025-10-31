babfc72d57c3cb09d0c1f5edb4c31d112873a15a
Waqar Ahmad <devwaqarahmad@gmail.com>
2025-10-31 18:52:26 +0500

    Explore Data

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import pandas as pd
 df= pd.read_csv("data/car_price_data.csv")
 
 print(df.head(3))
+# print("Shape:", df.shape)
+
+# print("\nInfo:")
+# print(df.info())
+
+# print("\nDescription:")
+# print(df.describe())
+
+# print("\nMISSING VALUES:")
+# print(df.isnull().sum())
+
+
+# print("\nUnique values in each column:")
+# for col in df.columns:
+#     print(f"{col}: {df[col].nunique()} unique")
